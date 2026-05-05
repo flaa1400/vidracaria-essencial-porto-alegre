6af3d918d0446673acdf5c122453f56a7fcb55f3
flaa1400 <fla141989@gmail.com>
2026-05-04 22:15:56 -0300

    Upload src/content/config.ts

---
+++ src/content/config.ts
@@ -0,0 +1,12 @@
+import { defineCollection, z } from 'astro:content';
+
+const servicos = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		icon: z.string().optional(),
+	}),
+});
+
+export const collections = { servicos };
